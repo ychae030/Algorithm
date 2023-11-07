f5016af7f2d5ca80efd7ce11c6c2815786fc7451
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-11-08 00:21:57 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,5 +1,4 @@
 function solution(num_list) {
     const [a,b] = num_list.slice(-2)
-    num_list.push(a < b ? b - a : b * 2)
-    return num_list
+    return [...num_list, a < b ? b - a : b * 2]
 }
