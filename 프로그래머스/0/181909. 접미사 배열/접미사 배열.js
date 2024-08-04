d61b4ac11036d0757b3146f8cac750758e771a49
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2024-08-04 22:53:14 +0900

    [level 0] Title: 접미사 배열, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,5 +1,6 @@
 function solution(my_string) {
-    const arr = [];
-    [...my_string].map((v,i) => arr.push(my_string.slice(i, my_string.length)))
-    return arr.sort()
+    // const arr = [];
+    // [...my_string].map((v,i) => arr.push(my_string.slice(i, my_string.length)))
+    // return arr.sort()
+    return [...my_string].map((v,i) => my_string.substring(i)).sort()
 }
